42d3b4dd8e3c336149af21347e8f8f3c582ce5a7
Дмитрий <safonovdmitr@gmail.com>
2019-05-31 23:04:33 +0300

    add locals

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -1,6 +1,16 @@
 ActiveAdmin.register Group do
   permit_params :department_id, :stream_id, :number
 
+  index do
+    id_column
+    column :number
+    column :department
+    column :stream
+    column :created_at
+    column :updated_at
+    actions
+  end
+
   form do |f|
     f.semantic_errors
     inputs :number, :department
