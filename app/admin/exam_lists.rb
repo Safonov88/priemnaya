c4324749f352ab5e4f425737ce087224f2010aca
Дмитрий <safonovdmitr@gmail.com>
2019-06-01 19:57:15 +0300

    fix

--- app/admin/exam_lists.rb
+++ app/admin/exam_lists.rb
@@ -2,6 +2,6 @@ ActiveAdmin.register ExamList do
   permit_params :entrant_id, :exam_id, :assessment
 
   filter :assessment
-  # filter :entrant, as: :select, collection: Entrant.pluck(:email, :id)
+  filter :entrant, as: :select, collection: Entrant.pluck(:email, :id)
   filter :exams
 end
