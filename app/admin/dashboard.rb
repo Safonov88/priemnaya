2c195adbec27a1f5794a09bd83eb4343e6dbb12d
Дмитрий <safonovdmitr@gmail.com>
2019-06-02 11:03:57 +0300

    fix

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,18 +4,28 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     columns do
       column do
-        panel "Recent Posts" do
-          ul do
-            Faculty.last(5).map do |post|
-              li link_to(post.name, admin_faculty_path(post))
-            end
-          end
+        panel "Абитуриентов на кафедрах" do
+          render "admin/department"
         end
       end
 
       column do
-        panel "Info" do
-          para "Welcome to ActiveAdmin."
+        panel "Абитуриентов в группах" do
+          render "admin/group"
+        end
+      end
+    end
+
+    columns do
+      column do
+        panel "Даты экзаменов" do
+          render "admin/exam"
+        end
+      end
+
+      column do
+        panel "Оценки" do
+          render "admin/assessment"
         end
       end
     end
